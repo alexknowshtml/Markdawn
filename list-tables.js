8c01d2125acdc10b07df8bc45034242d98b277ee
atharva-again <atharva.verma18@gmail.com>
2026-02-20 02:11:16 +0530

    chore: final verification and cleanup

--- list-tables.js
+++
@@ -1,9 +0,0 @@
-const { Pool } = require('pg');
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-pool.query("SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'").then(res => {
-  console.log(res.rows.map(r => r.table_name));
-  process.exit(0);
-}).catch(err => {
-  console.error(err);
-  process.exit(1);
-});
